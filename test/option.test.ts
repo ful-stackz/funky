0b4d0060ffb5956c485d90aa2e21157ebe870afe
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-05-26 12:06:06 +0200

    Add tests for option and result

---
+++ test/option.test.ts
@@ -0,0 +1,122 @@
+import {
+  assert,
+  assertEquals,
+  assertThrows,
+  fail,
+} from "https://deno.land/std/testing/asserts.ts";
+import { some, none } from "../mod.ts";
+
+const verifyOptionSome = (value: any): void => {
+  const option = some(value);
+
+  assert(option.isSome);
+  assertEquals(option.isNone, false);
+
+  option.match({
+    some: (val) => {
+      assertEquals(val, value);
+    },
+    none: () => {
+      fail();
+    }
+  });
+
+  option.matchSome((val) => {
+    assertEquals(val, value);
+  });
+
+  option.matchNone(() => {
+    fail();
+  });
+
+  assertEquals(option.unwrap(), value);
+  assertEquals(option.unwrapOr({} as any), value);
+};
+
+const verifyOptionNone = (): void => {
+  const option = none();
+
+  assert(option.isNone);
+  assertEquals(option.isSome, false);
+
+  option.match({
+    some: () => {
+      fail();
+    },
+    none: () => {
+      assert(true);
+    }
+  });
+
+  option.matchSome(() => {
+    fail();
+  });
+
+  option.matchNone(() => {
+    assert(true);
+  });
+
+  assertThrows(() => option.unwrap());
+  assertEquals(option.unwrapOr(42), 42);
+};
+
+Deno.test({
+  name: "Option<number>",
+  fn: () => {
+    const someNumbers = [
+      42,
+      0,
+      -1,
+      Math.PI,
+      Math.E,
+    ];
+    someNumbers.forEach(verifyOptionSome);
+  },
+});
+
+Deno.test({
+  name: "Option<string>",
+  fn: () => {
+    const someStrings = [
+      "",
+      "-1",
+      "false",
+      "undefined",
+    ];
+    someStrings.forEach(verifyOptionSome);
+  }
+});
+
+Deno.test({
+  name: "Option<boolean>",
+  fn: () => {
+    verifyOptionSome(true);
+    verifyOptionSome(false);
+  }
+});
+
+Deno.test({
+  name: "Option<array>",
+  fn: () => {
+    verifyOptionSome([]);
+    verifyOptionSome([-1]);
+  }
+});
+
+Deno.test({
+  name: "Option<object>",
+  fn: () => {
+    verifyOptionSome({
+      name: "funky",
+      descr: "Option, Result and more functional stuff"
+    });
+    verifyOptionSome({});
+  }
+});
+
+Deno.test({
+  name: "Option.none()",
+  fn: () => {
+    verifyOptionNone();
+  }
+});
---
+++ test/result.test.ts
@@ -0,0 +1,170 @@
+import {
+  assert,
+  assertEquals,
+  assertThrows,
+  fail,
+} from "https://deno.land/std/testing/asserts.ts";
+import { ok, err } from "../mod.ts";
+
+const verifyResultOk = (value: any): void => {
+  const result = ok(value);
+
+  assert(result.isOk);
+  assertEquals(result.isErr, false);
+
+  result.match({
+    ok: (val) => {
+      assertEquals(val, value);
+    },
+    err: () => {
+      fail();
+    }
+  });
+
+  result.matchOk((val) => {
+    assertEquals(val, value);
+  });
+
+  result.matchErr(() => {
+    fail();
+  });
+
+  assertEquals(result.unwrap(), value);
+  assertEquals(result.unwrapOr({} as any), value);
+  assertThrows(() => result.unwrapErr());
+};
+
+const verifyResultErr = (error: any): void => {
+  const result = err(error);
+
+  assert(result.isErr);
+  assertEquals(result.isOk, false);
+
+  result.match({
+    ok: () => {
+      fail();
+    },
+    err: (err) => {
+      assertEquals(err, error);
+    }
+  });
+
+  result.matchOk(() => {
+    fail();
+  });
+
+  result.matchErr((err) => {
+    assertEquals(err, error);
+  });
+
+  assertThrows(() => result.unwrap());
+  assertEquals(result.unwrapOr(42), 42);
+  assertEquals(result.unwrapErr(), error);
+};
+
+Deno.test({
+  name: "Result<number, never>",
+  fn: () => {
+    const someNumbers = [
+      42,
+      0,
+      -1,
+      Math.PI,
+      Math.E,
+    ];
+    someNumbers.forEach(verifyResultOk);
+  },
+});
+
+Deno.test({
+  name: "Result<string, never>",
+  fn: () => {
+    const someStrings = [
+      "",
+      "-1",
+      "false",
+      "undefined",
+    ];
+    someStrings.forEach(verifyResultOk);
+  }
+});
+
+Deno.test({
+  name: "Result<boolean, never>",
+  fn: () => {
+    verifyResultOk(true);
+    verifyResultOk(false);
+  }
+});
+
+Deno.test({
+  name: "Result<array, never>",
+  fn: () => {
+    verifyResultOk([]);
+    verifyResultOk([-1]);
+  }
+});
+
+Deno.test({
+  name: "Result<object, never>",
+  fn: () => {
+    verifyResultOk({
+      name: "funky",
+      descr: "Option, Result and more functional stuff"
+    });
+    verifyResultOk({});
+  }
+});
+
+Deno.test({
+  name: "Result<never, number>",
+  fn: () => {
+    const someNumbers = [
+      42,
+      0,
+      -1,
+      Math.PI,
+      Math.E,
+    ];
+    someNumbers.forEach(verifyResultErr);
+  }
+});
+
+Deno.test({
+  name: "Result<never, string>",
+  fn: () => {
+    const someStringErrors = [
+      "",
+      "-1",
+      "stringly error",
+    ];
+    someStringErrors.forEach(verifyResultErr);
+  }
+});
+
+Deno.test({
+  name: "Result<never, boolean>",
+  fn: () => {
+    verifyResultErr(true);
+    verifyResultErr(false);
+  }
+});
+
+Deno.test({
+  name: "Result<never, array>",
+  fn: () => {
+    verifyResultErr([]);
+    verifyResultErr([-1]);
+  }
+});
+
+Deno.test({
+  name: "Result<never, object>",
+  fn: () => {
+    verifyResultErr({
+      name: "error",
+      message: "Big bad error"
+    });
+    verifyResultErr({});
+  }
+});
